eac6d1750e371a810407d49f526d08b464f4775c
raeidsaqur <raeidsaqur@gmail.com>
2016-10-16 15:40:10 -0400

    Added Sample Project. Fixed compilation issues

---
+++ Example/PagingMenuController/PageMenuFramework/PageMenuFramework.h
@@ -0,0 +1,19 @@
+//
+//  PageMenuFramework.h
+//  PageMenuFramework
+//
+//  Created by Raeid Saqur on 2016-10-16.
+//  Copyright © 2016 Finatechal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PageMenuFramework.
+FOUNDATION_EXPORT double PageMenuFrameworkVersionNumber;
+
+//! Project version string for PageMenuFramework.
+FOUNDATION_EXPORT const unsigned char PageMenuFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PageMenuFramework/PublicHeader.h>
+
+
